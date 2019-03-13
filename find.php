8c3dc8e7e66b4a14883af491dfd325d84f0da843
sunhaosen <949993468@qq.com>
2019-03-13 11:29:25 +0800

    111

---
+++ find.php
@@ -0,0 +1,23 @@
+<?php
+$str='Have you ever gone shopping and';
+echo find($str);
+function find($str)
+{
+	$len = strlen($str);
+	$arr = [];
+	for ($i=0; $i < $len; $i++) 
+	{ 
+		if (isset($arr[$str[$i]])) 
+		{
+			$arr[$str[$i]]++;
+		}
+		else
+		{
+			$arr[$str[$i]] = 1;
+		}
+		if ($arr[$str[$i]] == 3) 
+		{
+			return $str[$i];
+		}
+	}
+}
