8b26f43b88c6fc9ff6752b64b282da9c8827491a
sunhaosen <949993468@qq.com>
2019-03-14 08:47:52 +0800

    day2

---
+++ 2.php
@@ -0,0 +1,21 @@
+<?php
+echo flower(1000);
+function flower($n)
+{
+	for ($i=1; $i <= $n; $i++) 
+	{ 
+		if (strlen($i) != 3) 
+		{
+			continue;
+		}
+		$a = floor($i/100);
+		$b = floor(($i%10)/10);
+		$c = $i%10;
+		$sum = pow($a, 3) + pow($b, 3) + pow($c, 3);
+		if ($sum == $i) 
+		{
+			echo $i;
+			echo "<br/>";
+		}
+	}
+}
